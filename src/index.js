a13d2544bc752ee3b2eb41c7eea3ab07a4ace761
Hanna <hanna.tylna@gmail.com>
2022-08-24 22:17:48 +0200

    add jquery

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import $ from 'jquery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+});
 root.render(
     <App />
 );
