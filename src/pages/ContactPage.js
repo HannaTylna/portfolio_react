6ff7a48aca52c833718c73aaeda0b67f60b0f126
Hanna <hanna.tylna@gmail.com>
2022-08-25 21:52:59 +0200

    add content to pages

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import Content from '../components/Content';
 
 export default function ContactPage(){
     return (
         <div className=''>
             <Navigation/>
+            <Content/>
             <Footer/>
         </div>
     )
