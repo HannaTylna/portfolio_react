c47d0c5b4589986401a0b342c0d166652b415366
Hanna <hanna.tylna@gmail.com>
2022-09-01 21:50:05 +0200

    change some styles

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,11 +2,16 @@ import React from "react";
 
 export default function Content() {
  return (
-  <div className="content">
-   <div className="container">
-    <div className="content__text">
-     <img src={require("../images/me.jpg")} alt="me" />
-    </div>
+  <div className="content__item">
+   <div className="content__image">
+    <img src={require("../images/me.jpg")} alt="me" />
+   </div>
+   <div className="content__body">
+    <p>
+     My name is Hanna Tylna. I am a beginner programmer. I like people and
+     coding. So I feel happy when I can do some code in a company of good
+     people. Perfect if the first one can result in something useful.
+    </p>
    </div>
   </div>
  );
